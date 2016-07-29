c99ea14e416139f7eed1ade4071b97d876b8e72d
jose123456879 <totitosst@gmail.com>
2016-07-29 08:43:44 -0600

    clases

---
+++ radio.py
@@ -0,0 +1,32 @@
+class radio():
+	def __init__(self, marca):
+		self.marca=marca
+		self.encendido=False
+		self.en_FM=true
+		self.emisora_AM=0
+		self.emisora_FM=88.0
+		self.volumen=0
+
+def encender(self):
+	self.encendido=true
+def apagar(self):
+	self.encendido=false
+def subir_volumen(self):
+	if self.volumen>=100:
+		self.volumen=5
+	else:
+	 	self.volumen+=5
+def bajar_volumen(self):
+	if self.volumen<=0:
+		self.volumen=0
+	else:
+		self.volumen-=5
+def subir_estacion(self):
+	if en_FM==true:
+		self.emisora_FM=0.5
+	else:	
+		self.emisora_AM=40
+	if 	
+
+
+		
